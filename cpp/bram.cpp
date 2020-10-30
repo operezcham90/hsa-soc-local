82ac5468485c4cb588876f7b4d3aac1abde6ebb0
operezcham <operezcham@gmail.com>
2020-10-30 17:16:27 -0600

    sum

--- cpp/bram.cpp
+++ cpp/bram.cpp
@@ -3,17 +3,22 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+// addresses from vivado block design
 off_t axi_bram_ctrl_0_addr = 0x40000000;
 off_t axi_bram_ctrl_1_addr = 0x42000000;
 off_t axi_gpio_0_addr = 0x41200000;
 off_t axi_gpio_1_addr = 0x41210000;
+off_t axi_gpio_2_addr = 0x41210000;
 long int *axi_bram_ctrl_0;
 long int *axi_bram_ctrl_1;
 long int *axi_gpio_0;
 long int *axi_gpio_1;
+long int *axi_gpio_2;
 int main()
 {
+    // size of bram default block
     unsigned int bram_size = 0x8000;
+    // 32 bits
     unsigned int gpio_size = 0x4;
     int fd;
     if ((fd = open("/dev/mem", O_RDWR | O_SYNC)) != -1)
@@ -22,38 +27,39 @@ int main()
         axi_bram_ctrl_1 = (long int *)mmap(NULL, bram_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, axi_bram_ctrl_1_addr);
         axi_gpio_0 = (long int *)mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, axi_gpio_0_addr);
         axi_gpio_1 = (long int *)mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, axi_gpio_1_addr);
+        axi_gpio_2 = (long int *)mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, axi_gpio_2_addr);
 
-        axi_bram_ctrl_0[0] = 1234;
-        printf("Read common\n");
-        printf("%ld\n", axi_bram_ctrl_1[0]);
+        // init BRAM
+        for (long int i = 0; i < 1000; i++)
+        {
+            axi_bram_ctrl_0[i] = i;
+        }
+        printf("Data written\n");
+        // confirm
+        int confirmed = 1;
+        for (long int i = 0; i < 1000; i++)
+        {
+            if (axi_bram_ctrl_1[i] != i)
+            {
+                confirmed = 0;
+            }
+        }
+        printf("Data read\n");
+        printf("Confirmed: %d\n", confirmed);
 
-        printf("Read low level\n");
-        axi_gpio_0[0] = 0;
-        printf("0: %ld\n", axi_gpio_1[0]);
-        axi_gpio_0[0] = 1;
-        printf("1: %ld\n", axi_gpio_1[0]);
-        axi_gpio_0[0] = 2;
-        printf("2: %ld\n", axi_gpio_1[0]);
-        axi_gpio_0[0] = 3;
-        printf("3: %ld\n", axi_gpio_1[0]);
-        axi_gpio_0[0] = 4;
-        printf("4: %ld\n", axi_gpio_1[0]);
-
-        axi_bram_ctrl_0[0] = 4231;
-        printf("Read common\n");
-        printf("%ld\n", axi_bram_ctrl_1[0]);
-
-        printf("Read low level\n");
-        axi_gpio_0[0] = 0;
-        printf("0: %ld\n", axi_gpio_1[0]);
-        axi_gpio_0[0] = 1;
-        printf("1: %ld\n", axi_gpio_1[0]);
-        axi_gpio_0[0] = 2;
-        printf("2: %ld\n", axi_gpio_1[0]);
-        axi_gpio_0[0] = 3;
-        printf("3: %ld\n", axi_gpio_1[0]);
-        axi_gpio_0[0] = 4;
-        printf("4: %ld\n", axi_gpio_1[0]);
+        printf("Sum\n");
+        // low level BRAM address is the number of the byte
+        int WAIT = 0b0;
+        int ADD = 0b1;
+        int RESET = 0b10;
+        axi_gpio_2[0] = RESET;
+        for (long int i = 0; i < 1000; i++)
+        {
+            axi_gpio_2[0] = WAIT;
+            axi_gpio_0[0] = i << 2;
+            printf("%ld\n", axi_gpio_1[0]);
+            axi_gpio_2[0] = ADD;
+        }
 
         close(fd);
     }
