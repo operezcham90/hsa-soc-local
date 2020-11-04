4b44c89381f1048f076a3323ad4d23a46936c6e4
operezcham <operezcham@gmail.com>
2020-11-03 21:35:03 -0600

    sum

--- cpp/bram.cpp
+++ cpp/bram.cpp
@@ -9,15 +9,11 @@ off_t axi_bram_ctrl_1_addr = 0x42000000;
 off_t axi_gpio_0_addr = 0x41200000;
 off_t axi_gpio_1_addr = 0x41210000;
 off_t axi_gpio_2_addr = 0x41220000;
-off_t axi_gpio_3_addr = 0x41230000;
-off_t axi_gpio_4_addr = 0x41240000;
 long int *axi_bram_ctrl_0;
 long int *axi_bram_ctrl_1;
 long int *axi_gpio_0;
 long int *axi_gpio_1;
 long int *axi_gpio_2;
-long int *axi_gpio_3;
-long int *axi_gpio_4;
 int main()
 {
     // size of bram default block
@@ -32,18 +28,16 @@ int main()
         axi_gpio_0 = (long int *)mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, axi_gpio_0_addr);
         axi_gpio_1 = (long int *)mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, axi_gpio_1_addr);
         axi_gpio_2 = (long int *)mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, axi_gpio_2_addr);
-        axi_gpio_3 = (long int *)mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, axi_gpio_3_addr);
-        axi_gpio_4 = (long int *)mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, axi_gpio_4_addr);
-
         // init BRAM
-        for (long int i = 0; i < 1000; i++)
+        int length = 1000;
+        for (long int i = 0; i < length; i++)
         {
             axi_bram_ctrl_0[i] = i;
         }
         printf("Data written\n");
         // verfy BRAM
         int confirmed = 1;
-        for (long int i = 0; i < 10; i++)
+        for (long int i = 0; i < length; i++)
         {
             if (axi_bram_ctrl_1[i] != i)
             {
@@ -52,35 +46,21 @@ int main()
         }
         printf("Data read\n");
         printf("Confirmed: %d\n", confirmed);
-
-        // verfy BRAM low level
-        confirmed = 1;
-        for (long int i = 0; i < 10; i++)
-        {
-            axi_gpio_0[0] = i << 2;
-            if (axi_gpio_3[0] != i)
-            {
-                confirmed = 0;
-            }
-        }
-        printf("Data read\n");
-        printf("Confirmed: %d\n", confirmed);
-
-        // Accumulator
-        long int WAIT = 0;
-        long int CLEAR = 2;
-        long int WORK = 1;
+        // Sum
+        long int WAIT = 0b00;
+        long int CLEAR = 0b10;
+        long int WORK = 0b01;
         axi_gpio_2[0] = CLEAR | WAIT;
         axi_gpio_2[0] = CLEAR | WORK;
-        printf("Value: %ld\n", axi_gpio_1[0]);
-        for (int i = 0; i < 100; i++)
+        printf("Start sum: %ld\n", axi_gpio_1[0]);
+        for (int i = 0; i < length; i++)
         {
-            axi_gpio_4[0] = i;
+            axi_gpio_0[0] = i << 2;
             axi_gpio_2[0] = WAIT;
             axi_gpio_2[0] = WORK;
-            printf("Value: %ld\n", axi_gpio_1[0]);
         }
-
+        printf("End sum: %ld\n", axi_gpio_1[0]);
+        printf("Expected sum: %ld\n", (length * (length + 1)) / 2;);
         close(fd);
     }
 }
