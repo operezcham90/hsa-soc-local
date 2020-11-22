aca3ad8c53fdb91fcd7d7ab39dab4960948ce7a3
operezcham <operezcham@gmail.com>
2020-11-22 17:16:10 -0600

    test

--- js/server.js
+++ js/server.js
@@ -4,7 +4,7 @@ const log = require('./log.js').log;
 const http = require('http');
 const fs = require('fs');
 http.createServer(function (req, res) {
-    let file = '/root/hsa-soc-local/html/error.html';
+    /*let file = '/root/hsa-soc-local/html/error.html';
     let type = 'text/html';
     if (req.url === '/') {
         file = '/root/hsa-soc-local/html/index.html';
@@ -19,7 +19,9 @@ http.createServer(function (req, res) {
         'Content-Length': stat.size
     });
     const rs = fs.createReadStream(file);
-    rs.pipe(res);
+    rs.pipe(res);*/
+    res.send(req.url);
+    res.end();
 }).listen(process.env.PORT);
 const url = `http://${ip}:${process.env.PORT}`;
 const message = `Listening on <a href=${url}>${url}</a>`;
