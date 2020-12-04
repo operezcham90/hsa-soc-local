f3f9a918f593ff06343cd6905250f1700f30f607
operezcham <operezcham@gmail.com>
2020-12-04 00:28:50 -0600

    download

--- js/opencv.js
+++ js/opencv.js
@@ -8,9 +8,19 @@ https.request(url, function (response) {
         data.push(chunk);
     });
     response.on('end', function () {
-        //fs.writeFileSync('/root/opencv_zynq7000.tar.gz', data.read());
         const html = data.read().toString();
         const redirect = html.replace('<html><body>You are being <a href="', '').replace('">redirected</a>.</body></html>', '');
-        console.log(redirect);
+        https.request(redirect, function (response) {
+            response.on('data', function (chunk) {
+                fs.appendFile('/root/opencv_zynq7000.tar.gz', chunk, function (err) {
+                    if (err) throw err;
+                    console.log('.')
+                });
+            });
+            response.on('end', function () {
+                //fs.writeFileSync('/root/opencv_zynq7000.tar.gz', data.read());
+                console.log('opencv_zynq7000.tar.gz ready');
+            });
+        }).end();
     });
 }).end();
