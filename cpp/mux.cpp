3c28dd6bc986da4250c50fca53b6f2ce8c5ed890
operezcham <operezcham@gmail.com>
2020-12-07 13:25:18 -0600

    mux

--- cpp/mux.cpp
+++ cpp/mux.cpp
@@ -6,12 +6,8 @@
 // addresses from vivado block design
 off_t axi_gpio_0_addr = 0x41200000;
 off_t axi_gpio_1_addr = 0x41210000;
-off_t axi_gpio_2_addr = 0x41220000;
-off_t axi_gpio_3_addr = 0x41230000;
 long int *axi_gpio_0;
 long int *axi_gpio_1;
-long int *axi_gpio_2;
-long int *axi_gpio_3;
 int main()
 {
     // 32 bits
@@ -21,20 +17,23 @@ int main()
     {
         axi_gpio_0 = (long int *)mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, axi_gpio_0_addr);
         axi_gpio_1 = (long int *)mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, axi_gpio_1_addr);
-        axi_gpio_2 = (long int *)mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, axi_gpio_2_addr);
-        axi_gpio_3 = (long int *)mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, axi_gpio_3_addr);
-
         //A
-        axi_gpio_0[0] = 1111;
+        long int A = 0x0001;
         //B
-        axi_gpio_1[0] = 2222;
-        for (long int i = 0; i < 40; i++)
-        {
-            //selection
-            axi_gpio_2[0] = i;
-            //output
-            printf("mux: %ld\n", axi_gpio_3[0]);
-        }
+        long int B = 0x0200;
+        // selA
+        long int selA = 0;
+        long int selB = 0b10000000000000000;
+
+        // write
+        axi_gpio_0[0] = A | B | selA;
+        // read
+        printf("mux: %ld\n", axi_gpio_1[0]);
+
+        // write
+        axi_gpio_0[0] = A | B | selB;
+        // read
+        printf("mux: %ld\n", axi_gpio_1[0]);
 
         close(fd);
     }
