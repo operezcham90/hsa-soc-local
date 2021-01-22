a345d8601ecf4e489146fa5fdf8d2680125eafc8
operezcham <operezcham@gmail.com>
2021-01-22 12:35:48 -0600

    counter

---
+++ cpp/counter.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+#include <vector>
+using namespace std;
+using namespace std::chrono;
+// AXI pointers
+unsigned int gpio_bytes = 4;
+long int *limit;
+long int *control;
+long int *count;
+long int *accumulated;
+// open memory
+long int *map_mem(unsigned int bytes, off_t addr)
+{
+    return (long int *)mmap(NULL, bytes, PROT_READ | PROT_WRITE, MAP_SHARED, fd, addr);
+}
+int open_mem()
+{
+    if ((fd = open("/dev/mem", O_RDWR | O_SYNC)) == -1)
+    {
+        exit(1);
+    }
+    limit = map_mem(gpio_bytes, 0x41200000);
+    control = map_mem(gpio_bytes, 0x41200000);
+    count = map_mem(gpio_bytes, 0x41200000);
+    accumulated = map_mem(gpio_bytes, 0x41230000);
+}
+int close_mem()
+{
+    close(fd);
+}
+int main()
+{
+
+    open_mem();
+    long int none = 0b0;
+    long int work = 0b1;
+    long int clear = 0b10;
+    long int square = 0b100;
+
+    limit[0] = 10;
+    cout << "*Limit " << limit[0] << "\n";
+
+    control[0] = clear;
+    cout << "Clear\n";
+    cout << "Count " << count[0] << "\n";
+    cout << "Accumulated " << accumulated[0] << "\n";
+
+    control[0] = work;
+    cout << "Work\n";
+    while (true)
+    {
+        cout << "Count " << count[0] << "\n";
+        cout << "Accumulated " << accumulated[0] << "\n";
+    }
+
+    close_mem();
+    return 0;
+}
