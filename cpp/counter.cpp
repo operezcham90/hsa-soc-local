c7233b1c3220c08763eb2e3152eb4f3b0900ed55
operezcham <operezcham@gmail.com>
2021-01-24 15:00:56 -0600

    mux

--- cpp/counter.cpp
+++ cpp/counter.cpp
@@ -15,12 +15,12 @@ using namespace std::chrono;
 // AXI pointers
 unsigned int gpio_bytes = 4;
 unsigned int bram_bytes = 2048 * 4;
-long int *limit;
-long int *control;
-long int *counter;
-long int *accumulated;
-long int *version;
-long int *average;
+long int *lim;
+long int *ctrl;
+long int *count;
+long int *acc;
+long int *ver;
+long int *avg;
 unsigned char *img1_bram;
 int fd;
 // open memory
@@ -32,14 +32,7 @@ int send_img()
 {
     for (int i = 0; i < bram_bytes; i++)
     {
-        if (i % 2 == 0)
-        {
-            img1_bram[i] = 1;
-        }
-        else
-        {
-            img1_bram[i] = 3;
-        }
+        img1_bram[i] = i % 0x100;
     }
 }
 int open_mem()
@@ -48,12 +41,12 @@ int open_mem()
     {
         exit(1);
     }
-    limit = map_mem(gpio_bytes, 0x41200000);
-    control = map_mem(gpio_bytes, 0x41210000);
-    counter = map_mem(gpio_bytes, 0x41220000);
-    accumulated = map_mem(gpio_bytes, 0x41230000);
-    version = map_mem(gpio_bytes, 0x41240000);
-    average = map_mem(gpio_bytes, 0x41250000);
+    lim = map_mem(gpio_bytes, 0x41200000);
+    ctrl = map_mem(gpio_bytes, 0x41210000);
+    count = map_mem(gpio_bytes, 0x41220000);
+    acc = map_mem(gpio_bytes, 0x41230000);
+    ver = map_mem(gpio_bytes, 0x41240000);
+    avg = map_mem(gpio_bytes, 0x41250000);
     img1_bram = (unsigned char *)map_mem(bram_bytes, 0x40000000);
 }
 int close_mem()
@@ -62,48 +55,48 @@ int close_mem()
 }
 int print_version()
 {
-    cout << "PL VER " << version[0] << "\n";
+    cout << "ver " << ver[0] << "\n";
 }
-int set_limit(long int lim)
+int set_limit(long int limit)
 {
-    limit[0] = lim;
-    cout << "limit " << limit[0] << "\n";
+    lim[0] = limit;
+    cout << "lim " << lim[0] << "\n";
 }
-int set_average(long int avg)
+int set_average(long int average)
 {
-    average[0] = avg;
-    cout << "average " << average[0] << "\n";
+    avg[0] = average;
+    cout << "avg " << avg[0] << "\n";
 }
 int wait_clear()
 {
-    control[0] = 0b10;
-    while (counter[0] + accumulated[0] > 0)
+    ctrl[0] = 0b10;
+    while (count[0] | acc[0] > 0)
     {
     }
-    control[0] = 0b0;
+    ctrl[0] = 0b0;
 }
 int start_work(int square)
 {
     if (square)
     {
-        control[0] = 0b101;
+        ctrl[0] = 0b101;
     }
     else
     {
-        control[0] = 0b1;
+        ctrl[0] = 0b1;
     }
 }
 int wait_work()
 {
-    while (counter[0] < limit[0])
+    while (count[0] < lim[0])
     {
     }
-    control[0] = 0b0;
+    ctrl[0] = 0b0;
 }
 int print_result()
 {
-    cout << "count " << counter[0] << "\n";
-    cout << "acc " << accumulated[0] << "\n";
+    cout << "count " << count[0] << "\n";
+    cout << "acc " << acc[0] << "\n";
 }
 int main()
 {
@@ -113,15 +106,14 @@ int main()
     auto start = high_resolution_clock::now();
     // send work data
     send_img();
-    set_limit(1000);
-    set_average(0);
+    set_limit(255);
     // work
     wait_clear();
     start_work(0);
     wait_work();
     print_result();
     // send work data
-    set_limit(1000);
+    set_limit(255);
     set_average(2);
     // work
     wait_clear();
