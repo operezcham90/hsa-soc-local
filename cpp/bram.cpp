2a062358bb1ff5e160383f8caa0e585d97521fe8
operezcham <operezcham@gmail.com>
2021-02-12 17:59:43 -0600

    standalone

--- cpp/bram.cpp
+++ cpp/bram.cpp
@@ -6,6 +6,7 @@
 // addresses from vivado block design
 long int *axi_bram_ctrl_0;
 long int *axi_gpio_0;
+long int *axi_gpio_1;
 int main()
 {
     // size of bram default block
@@ -17,19 +18,26 @@ int main()
     {
         axi_bram_ctrl_0 = (long int *)mmap(NULL, bram_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x40000000);
         axi_gpio_0 = (long int *)mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x41200000);
+        axi_gpio_1 = (long int *)mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x41210000);
         unsigned char *axi_bram_ctrl_0_bytes = (unsigned char *)axi_bram_ctrl_0;
 
-        printf("Count: %ld\n", axi_gpio_0[0]);
-        printf("Count: %ld\n", axi_gpio_0[0]);
-        printf("Count: %ld\n", axi_gpio_0[0]);
         axi_bram_ctrl_0_bytes[0] = 1;
-        printf("Count: %ld\n", axi_gpio_0[0]);
-        printf("Count: %ld\n", axi_gpio_0[0]);
-        printf("Count: %ld\n", axi_gpio_0[0]);
-        axi_bram_ctrl_0_bytes[0] = 0;
-        printf("Count: %ld\n", axi_gpio_0[0]);
-        printf("Count: %ld\n", axi_gpio_0[0]);
-        printf("Count: %ld\n", axi_gpio_0[0]);
+        axi_bram_ctrl_0_bytes[1] = 2;
+        axi_bram_ctrl_0_bytes[2] = 3;
+        axi_bram_ctrl_0_bytes[3] = 4;
+
+        axi_gpio_0[0] = 0;
+        printf("Count: %ld\n", axi_gpio_1[0]);
+
+        axi_gpio_0[0] = 1;
+        printf("Count: %ld\n", axi_gpio_1[0]);
+
+        axi_gpio_0[0] = 2;
+        printf("Count: %ld\n", axi_gpio_1[0]);
+
+        axi_gpio_0[0] = 3;
+        printf("Count: %ld\n", axi_gpio_1[0]);
+
         close(fd);
     }
 }
