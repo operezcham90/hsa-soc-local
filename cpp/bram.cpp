a4aec405a38ac8cd5cefdfb94e70fd3fde3055e7
operezcham <operezcham@gmail.com>
2021-02-13 13:46:05 -0600

    test

--- cpp/bram.cpp
+++ cpp/bram.cpp
@@ -40,6 +40,7 @@ int main()
         axi_gpio_0[0] = 3;
         printf("Count: %ld\n", axi_gpio_1[0]);*/
 
+        axi_bram_ctrl_0[1] = 10000;
         axi_bram_ctrl_0[0] = 0;
         axi_gpio_0[0] = 1;
         printf("Count: %lu Len: %lu\n", axi_gpio_1[0], axi_gpio_2[0]);
