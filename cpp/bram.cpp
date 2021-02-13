560011134ae6f6fbf79ebd5dc0a877b8ebb5c905
operezcham <operezcham@gmail.com>
2021-02-13 01:38:50 -0600

    wait

--- cpp/bram.cpp
+++ cpp/bram.cpp
@@ -4,9 +4,9 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 // addresses from vivado block design
-long int *axi_bram_ctrl_0;
-long int *axi_gpio_0;
-long int *axi_gpio_1;
+unsigned long int *axi_bram_ctrl_0;
+unsigned long int *axi_gpio_0;
+unsigned long int *axi_gpio_1;
 int main()
 {
     // size of bram default block
@@ -16,12 +16,12 @@ int main()
     int fd;
     if ((fd = open("/dev/mem", O_RDWR | O_SYNC)) != -1)
     {
-        axi_bram_ctrl_0 = (long int *)mmap(NULL, bram_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x40000000);
-        axi_gpio_0 = (long int *)mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x41200000);
-        axi_gpio_1 = (long int *)mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x41210000);
+        axi_bram_ctrl_0 = (unsigned long int *)mmap(NULL, bram_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x40000000);
+        axi_gpio_0 = (unsigned long int *)mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x41200000);
+        axi_gpio_1 = (unsigned long int *)mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x41210000);
         unsigned char *axi_bram_ctrl_0_bytes = (unsigned char *)axi_bram_ctrl_0;
 
-        axi_bram_ctrl_0_bytes[0] = 4;
+        /*axi_bram_ctrl_0_bytes[0] = 4;
         axi_bram_ctrl_0_bytes[1] = 3;
         axi_bram_ctrl_0_bytes[2] = 2;
         axi_bram_ctrl_0_bytes[3] = 1;
@@ -36,6 +36,19 @@ int main()
         printf("Count: %ld\n", axi_gpio_1[0]);
 
         axi_gpio_0[0] = 3;
+        printf("Count: %ld\n", axi_gpio_1[0]);*/
+
+        printf("Count: %ld\n", axi_gpio_1[0]);
+        printf("Count: %ld\n", axi_gpio_1[0]);
+        printf("Count: %ld\n", axi_gpio_1[0]);
+        axi_bram_ctrl_0[0] = 10000;
+        printf("Count: %ld\n", axi_gpio_1[0]);
+        printf("Count: %ld\n", axi_gpio_1[0]);
+        printf("Count: %ld\n", axi_gpio_1[0]);
+        axi_gpio_0[0] = 1;
+        axi_gpio_0[0] = 0;
+        printf("Count: %ld\n", axi_gpio_1[0]);
+        printf("Count: %ld\n", axi_gpio_1[0]);
         printf("Count: %ld\n", axi_gpio_1[0]);
 
         close(fd);
