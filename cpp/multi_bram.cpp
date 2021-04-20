c1f61caacbe72b721c1cc2791db6bb5d401bef6f
operezcham <operezcham@gmail.com>
2021-04-19 22:17:43 -0500

    own

--- cpp/multi_bram.cpp
+++ cpp/multi_bram.cpp
@@ -47,29 +47,15 @@ void *task4(void *arg)
     memcpy(axi_bram_ctrl_3, data4, bram_size);
     return NULL;
 }
-void *memcpy_own(void *dstpp, const void *srcpp, size_t len)
+void memcpy_own(void *dest, void *src, size_t n)
 {
-    unsigned long int dstp = (long int)dstpp;
-    unsigned long int srcp = (long int)srcpp;
-    /* Copy from the beginning to the end.  */
-    /* If there not too few bytes to copy, use word copy.  */
-    if (len >= OP_T_THRES)
-    {
-        /* Copy just a few bytes to make DSTP aligned.  */
-        len -= (-dstp) % OPSIZ;
-        BYTE_COPY_FWD(dstp, srcp, (-dstp) % OPSIZ);
-        /* Copy whole pages from SRCP to DSTP by virtual address manipulation,
-         as much as possible.  */
-        PAGE_COPY_FWD_MAYBE(dstp, srcp, len, len);
-        /* Copy from SRCP to DSTP taking advantage of the known alignment of
-         DSTP.  Number of bytes remaining is put in the third argument,
-         i.e. in LEN.  This number may vary from machine to machine.  */
-        WORD_COPY_FWD(dstp, srcp, len, len);
-        /* Fall out and copy the tail.  */
-    }
-    /* There are just a few bytes to copy.  Use byte memory operations.  */
-    BYTE_COPY_FWD(dstp, srcp, len);
-    return dstpp;
+    // Typecast src and dest addresses to (char *)
+    char *csrc = (char *)src;
+    char *cdest = (char *)dest;
+
+    // Copy contents of src[] to dest[]
+    for (int i = 0; i < n; i++)
+        cdest[i] = csrc[i];
 }
 int main()
 {
