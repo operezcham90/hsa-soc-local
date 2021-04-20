a3a2928dd0301a90715ad3794de882a14f6d2d57
operezcham <operezcham@gmail.com>
2021-04-20 17:07:29 -0500

    mp

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -1,10 +1,23 @@
 // g++ hello.cpp -fopenmp -o hello
 #include <iostream>
+#include <sched.h>
+#include <omp.h>
+
+int done = 0;
+
 int main()
 {
 #pragma omp parallel
     {
-        std::cout << "Hello World" << std::endl;
+        int thread_num = omp_get_thread_num();
+        int cpu_num = sched_getcpu();
+
+        while (cpu_num != done)
+        {
+            std::cout << thread_num << "running in " << cpu_num << std::endl;
+            done++;
+        }
     }
+
     return 0;
 }
