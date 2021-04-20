93e05d6dc0a319239259801e18354e2e94c3f990
operezcham <operezcham@gmail.com>
2021-04-20 15:35:08 -0500

    parallel

--- cpp/multi_bram.cpp
+++ cpp/multi_bram.cpp
@@ -23,71 +23,116 @@ unsigned int bram_size = 2048 * 4;
 unsigned int half_bram_size = 2048 * 2;
 int brams = 0;
 
-/*void *task1(void *arg)
+void *task0(void *arg)
 {
-    memcpy(axi_bram_ctrl_0_bytes, data_bytes, half_bram_size);
-    memcpy(axi_bram_ctrl_2_bytes, data3_bytes, half_bram_size);
-    memcpy(axi_bram_ctrl_1_bytes, data2_bytes, half_bram_size);
-    memcpy(axi_bram_ctrl_3_bytes, data4_bytes, half_bram_size);
+    int fd;
+    if ((fd = open("/dev/mem", O_RDWR | O_SYNC)) != -1)
+    {
+        // alloc pointers to brams
+        axi_bram_ctrl = (unsigned long int **)malloc(brams * sizeof(unsigned long int *));
+
+        // alloc pointers to data sources
+        data = (unsigned long int **)malloc(brams * sizeof(unsigned long int *));
+
+        // map brams
+        for (int i = 0; i < brams; i++)
+        {
+            off_t addr = 0x40000000 + 0x2000000 * i;
+            axi_bram_ctrl[i] = (unsigned long int *)mmap(NULL, bram_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, addr);
+            /*cout << "BRAM at ";
+            cout << hex << addr;
+            cout << " ready\n";*/
+            data[i] = (unsigned long int *)malloc(half_bram_size);
+        }
+
+        // set first data source
+        for (unsigned long int i = 0; i < half_bram_size / 4; i++)
+        {
+            data[0][i] = i;
+        }
+
+        // make copies to other data sources
+        for (int i = 1; i < brams; i++)
+        {
+            memcpy(data[i], data[0], half_bram_size);
+        }
+
+        // copy data to bram
+        for (int i = 0; i < brams; i++)
+        {
+            unsigned char *dest = (unsigned char *)axi_bram_ctrl[i];
+            memcpy(dest, data[i], half_bram_size);
+        }
+
+        close(fd);
+    }
     return NULL;
 }
-void *task2(void *arg)
+void *task1(void *arg)
 {
-    memcpy(axi_bram_ctrl_1_bytes + half_bram_size, data2_bytes + half_bram_size, half_bram_size);
-    memcpy(axi_bram_ctrl_3_bytes + half_bram_size, data4_bytes + half_bram_size, half_bram_size);
-    memcpy(axi_bram_ctrl_0_bytes + half_bram_size, data_bytes + half_bram_size, half_bram_size);
-    memcpy(axi_bram_ctrl_2_bytes + half_bram_size, data3_bytes + half_bram_size, half_bram_size);
+    int fd;
+    if ((fd = open("/dev/mem", O_RDWR | O_SYNC)) != -1)
+    {
+        // alloc pointers to brams
+        axi_bram_ctrl = (unsigned long int **)malloc(brams * sizeof(unsigned long int *));
+
+        // alloc pointers to data sources
+        data = (unsigned long int **)malloc(brams * sizeof(unsigned long int *));
+
+        // map brams
+        for (int i = 0; i < brams; i++)
+        {
+            off_t addr = 0x40000000 + 0x2000000 * i;
+            axi_bram_ctrl[i] = (unsigned long int *)mmap(NULL, bram_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, addr);
+            /*cout << "BRAM at ";
+            cout << hex << addr;
+            cout << " ready\n";*/
+            data[i] = (unsigned long int *)malloc(half_bram_size);
+        }
+
+        // set first data source
+        for (unsigned long int i = half_bram_size / 4; i < bram_size / 4; i++)
+        {
+            data[0][i] = i;
+        }
+
+        // make copies to other data sources
+        for (int i = 1; i < brams; i++)
+        {
+            memcpy(data[i], data[0], half_bram_size);
+        }
+
+        // copy data to bram
+        for (int i = 0; i < brams; i++)
+        {
+            unsigned char *dest = (unsigned char *)axi_bram_ctrl[i];
+            memcpy(dest + half_bram_size, data[i], half_bram_size);
+        }
+
+        close(fd);
+    }
     return NULL;
 }
 void parallel_copy(int num)
 {
-    pthread_t a1;
-
-    pthread_t b1;
-    pthread_t b2;
-
-    pthread_t c1;
-    pthread_t c2;
-    pthread_t c3;
-
-    pthread_t d1;
-    pthread_t d2;
-    pthread_t d3;
-    pthread_t d4;
-
-    pthread_attr_t attr0;
-    pthread_attr_t attr1;
-    cpu_set_t cpus0;
-    cpu_set_t cpus1;
+    brams = num;
+    pthread_t t0;
+    pthread_t t1;
+    pthread_attr_t attr;
+    cpu_set_t cpus;
     sched_param params;
-    params.sched_priority = 5;
-    pthread_attr_init(&attr0);
-    pthread_attr_init(&attr1);
-    CPU_ZERO(&cpus0);
-    CPU_ZERO(&cpus1);
-    CPU_SET(0, &cpus0);
-    CPU_SET(1, &cpus1);
-    pthread_attr_setaffinity_np(&attr0, sizeof(cpu_set_t), &cpus0);
-    pthread_attr_setaffinity_np(&attr1, sizeof(cpu_set_t), &cpus1);
-    pthread_attr_setschedparam(&attr0, &params);
-    pthread_attr_setschedparam(&attr1, &params);
-
-    start = high_resolution_clock::now();
-    pthread_create(&a1, &attr0, task1, NULL);
-    pthread_join(a1, NULL);
-    stop = high_resolution_clock::now();
-    duration = duration_cast<microseconds>(stop - start);
-    cout << "Write 1 BRAM: " << duration.count() << " us\n";
-
-    start = high_resolution_clock::now();
-    pthread_create(&b1, &attr0, task1, NULL);
-    pthread_create(&b2, &attr1, task2, NULL);
-    pthread_join(b1, NULL);
-    pthread_join(b2, NULL);
-    stop = high_resolution_clock::now();
-    duration = duration_cast<microseconds>(stop - start);
-    cout << "Write 2 BRAM: " << duration.count() << " us\n";
-}*/
+    params.sched_priority = 1000;
+    pthread_attr_init(&attr);
+    CPU_ZERO(&cpus);
+    CPU_SET(0, &cpus);
+    CPU_SET(1, &cpus);
+    pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
+    pthread_attr_setschedparam(&attr, &params);
+    pthread_create(&t0, &attr, task0, NULL);
+    pthread_create(&t1, &attr, task1, NULL);
+    pthread_join(t0, NULL);
+    pthread_join(t1, NULL);
+}
 void sequential_copy(int num)
 {
     brams = num;
@@ -141,6 +186,15 @@ int main()
         auto stop = high_resolution_clock::now();
         auto duration = duration_cast<microseconds>(stop - start);
         cout << dec << "Seq time " << i << ": " << duration.count() << " us\n";
+    }
+
+    for (int i = 1; i <= 16; i++)
+    {
+        auto start = high_resolution_clock::now();
+        parallel_copy(i);
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+        cout << dec << "Par time " << i << ": " << duration.count() << " us\n";
     }
 
     return 0;
