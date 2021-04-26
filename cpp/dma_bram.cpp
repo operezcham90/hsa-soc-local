a28f863aa9fd83e0c374eb1ffa66347b2bbf304f
operezcham <operezcham@gmail.com>
2021-04-26 17:03:13 -0500

    reset

--- cpp/dma_bram.cpp
+++ cpp/dma_bram.cpp
@@ -19,23 +19,24 @@ int main()
     unsigned int *cdma = (unsigned int *)mmap(NULL, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, dh, 0x7E200000);
     unsigned int *ddr_src = (unsigned int *)mmap(NULL, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, dh, 0x0e000000);
     unsigned int *ddr_dest = (unsigned int *)mmap(NULL, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, dh, 0x0f000000);
-    if ((cdma[1] & 0x00000002) != 0)
+    // reset CDMA
+    cout << "CDMA reset\n";
+    while ((cdma[1] & 0x00000002) == 0)
     {
-        cout << "CDMA in idle state";
+        cdma[0] = 0x00000004;
     }
-    else
-    {
-        cout << "CDMA not in idle state";
-    }
+    cout << "CDMA in idle state\n";
+    // write source
     for (int i = 0; i < 8; i++)
     {
         ddr_src[i] = i + 1;
     }
-    cout << "\nSRC data:\n";
+    cout << "SRC data:\n";
     for (int i = 0; i < 8; i++)
     {
         cout << hex << ddr_src[i] << " ";
     }
+    // write destiny
     for (int i = 0; i < 8; i++)
     {
         ddr_dest[i] = 0;
