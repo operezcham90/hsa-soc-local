930999419deb6341300441a4bfe8149aa5d2b892
operezcham <operezcham@gmail.com>
2021-04-26 16:47:21 -0500

    ground

--- cpp/dma_bram.cpp
+++ cpp/dma_bram.cpp
@@ -9,27 +9,35 @@ using namespace std;
 
 int main()
 {
-    int map_size = 65535;
     int dh = open("/dev/mem", O_RDWR | O_SYNC);
     if (dh == -1)
     {
         cout << "devmem fail\n";
+        return 1;
     }
+    int map_size = 0x10000; // 64K
     unsigned int *cdma = (unsigned int *)mmap(NULL, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, dh, 0x7E200000);
     unsigned int *ddr_src = (unsigned int *)mmap(NULL, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, dh, 0x0e000000);
     unsigned int *ddr_dest = (unsigned int *)mmap(NULL, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, dh, 0x0f000000);
-    cout << "CDMA:\n";
-    for (int i = 0; i < 2; i++)
+    if (cdma[1] & 0x2 == 0x2)
     {
-        cout << hex << cdma[i] << " ";
+        cout << "CDMA in idle state";
     }
-    cout << "SRC:\n";
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < 8; i++)
+    {
+        ddr_src[i] = i;
+    }
+    cout << "\nSRC data:\n";
+    for (int i = 0; i < 8; i++)
     {
         cout << hex << ddr_src[i] << " ";
     }
-    cout << "DEST:\n";
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < 8; i++)
+    {
+        ddr_dest[i] = 0;
+    }
+    cout << "\nDEST:\n";
+    for (int i = 0; i < 2; i++)
     {
         cout << hex << ddr_dest[i] << " ";
     }
