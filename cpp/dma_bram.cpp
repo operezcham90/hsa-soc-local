d08d03fe5eaf3aa72cf59878d25bc86e4fe2307f
operezcham <operezcham@gmail.com>
2021-04-26 14:29:48 -0500

    restore

--- cpp/dma_bram.cpp
+++ cpp/dma_bram.cpp
@@ -16,9 +16,9 @@
 
 unsigned long int CDMA = 0x7E200000;
 unsigned long int RAM = 0x10000000;
-unsigned long int BRAM = 0x10008000;
+unsigned long int BRAM = 0x10001000;
 
-unsigned long int FILESIZE = 16384;
+unsigned long int FILESIZE = 1;
 
 int main(int argc, char *argv[])
 {
@@ -32,23 +32,10 @@ int main(int argc, char *argv[])
 
     int fd = open("/dev/mem", O_RDWR | O_SYNC);
     void *cdma = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, CDMA);
-    void *bram = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, BRAM);
+    void *bram = mmap(0, 8 * MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, BRAM);
     void *ram = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, RAM);
 
     printf("BRAM contents: ");
-    for (i = 0; i < 20; i++)
-    {
-        printf("%d, ", ((volatile unsigned int *)bram)[i]);
-    }
-    printf("\n");
-
-    printf("Writing test stuff to the BRAM.\n");
-    for (i = 0; i < 1024; i++)
-    {
-        ((volatile unsigned int *)bram)[i] = ((unsigned int)i);
-    }
-
-    printf("some new BRAM contents: ");
     for (i = 0; i < 20; i++)
     {
         printf("%d, ", ((volatile unsigned int *)bram)[i]);
@@ -75,7 +62,7 @@ int main(int argc, char *argv[])
     value = *(volatile unsigned int *)(cdma + 0x00);
     printf("Control Register = 0x%08x\n", value);
 
-    *(volatile unsigned int *)(cdma + 0x18) = (volatile unsigned int)(0x10008000);
+    *(volatile unsigned int *)(cdma + 0x18) = (volatile unsigned int)(0xC0000000);
     value = *(volatile unsigned int *)(cdma + 0x18);
     printf("Source Pointer = 0x%08x\n", value);
 
@@ -84,7 +71,7 @@ int main(int argc, char *argv[])
     printf("Destination Pointer = 0x%08x\n", value);
 
     //	*(volatile unsigned int *)(cdma + 0x28) = ((volatile unsigned int)0x00004000);
-    *(volatile unsigned int *)(cdma + 0x28) = ((volatile unsigned int)FILESIZE);
+    *(volatile unsigned int *)(cdma + 0x28) = ((volatile unsigned int)4096 * FILESIZE);
     value = *(volatile unsigned int *)(cdma + 0x28);
     printf("Bytes to Transfer (BTT) = 0x%08x\n", value);
 
@@ -114,7 +101,7 @@ int main(int argc, char *argv[])
     FILE *fp;
     unsigned int val;
     fp = fopen("out.txt", "w");
-    for (i = 0; i < (FILESIZE / 4); i++)
+    for (i = 0; i < ((4096 * FILESIZE) / 4); i++)
     {
         fprintf(fp, "%u,", ((volatile unsigned int *)ram)[i]);
     }
