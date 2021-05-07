9c66a629e67c5975c4f5fec296df207d9783ce4c
operezcham <operezcham@gmail.com>
2021-05-07 14:17:33 -0500

    constant

--- cpp/dma_opencv.cpp
+++ cpp/dma_opencv.cpp
@@ -32,10 +32,10 @@ int main()
     // open cdma
     start = high_resolution_clock::now();
     int dh = open("/dev/mem", O_RDWR | O_SYNC);
-    cdma_0 = (unsigned long *)mmap(NULL, 40, PROT_READ | PROT_WRITE, MAP_SHARED, dh, axi_cdma_0);
-    cdma_1 = (unsigned long *)mmap(NULL, 40, PROT_READ | PROT_WRITE, MAP_SHARED, dh, axi_cdma_1);
-    cdma_2 = (unsigned long *)mmap(NULL, 40, PROT_READ | PROT_WRITE, MAP_SHARED, dh, axi_cdma_2);
-    cdma_3 = (unsigned long *)mmap(NULL, 40, PROT_READ | PROT_WRITE, MAP_SHARED, dh, axi_cdma_3);
+    cdma_0 = (unsigned long *)mmap(NULL, 40, PROT_READ | PROT_WRITE, MAP_SHARED, dh, 0x7E200000);
+    cdma_1 = (unsigned long *)mmap(NULL, 40, PROT_READ | PROT_WRITE, MAP_SHARED, dh, 0x7E210000);
+    cdma_2 = (unsigned long *)mmap(NULL, 40, PROT_READ | PROT_WRITE, MAP_SHARED, dh, 0x7E220000);
+    cdma_3 = (unsigned long *)mmap(NULL, 40, PROT_READ | PROT_WRITE, MAP_SHARED, dh, 0x7E230000);
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     cout << dec << "Open: " << duration.count() << " us\n";
