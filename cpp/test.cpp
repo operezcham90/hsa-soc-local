eafc3236fcafb2c22bebcc13dab8ce2013b63ef9
operezcham <operezcham@gmail.com>
2021-07-10 17:01:50 -0500

    8 units

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -33,6 +33,14 @@
 #define DDR_6_ADDR 0x14000000
 #define DDR_7_ADDR 0x15000000
 #define DDR_8_ADDR 0x16000000
+#define DDR_9_ADDR 0x17000000
+#define DDR_10_ADDR 0x18000000
+#define DDR_11_ADDR 0x19000000
+#define DDR_12_ADDR 0x1A000000
+#define DDR_13_ADDR 0x1B000000
+#define DDR_14_ADDR 0x1C000000
+#define DDR_15_ADDR 0x1D000000
+#define DDR_16_ADDR 0x1E000000
 #define BRAM_0_ADDR 0xC0000000
 #define BRAM_1_ADDR 0xC2000000
 #define BRAM_2_ADDR 0xC4000000
@@ -42,6 +50,14 @@
 #define BRAM_6_ADDR 0xC2000000
 #define BRAM_7_ADDR 0xC0000000
 #define BRAM_8_ADDR 0xC2000000
+#define BRAM_9_ADDR 0xC6000000
+#define BRAM_10_ADDR 0xC8000000
+#define BRAM_11_ADDR 0xC4000000
+#define BRAM_12_ADDR 0xC6000000
+#define BRAM_13_ADDR 0xC4000000
+#define BRAM_14_ADDR 0xC6000000
+#define BRAM_15_ADDR 0xC4000000
+#define BRAM_16_ADDR 0xC6000000
 #define CLEAR_CDMA 0x4
 #define DONE_CDMA 0x2
 #define STANDBY_CDMA 0x0
@@ -56,10 +72,18 @@ unsigned char *data_i_0;
 unsigned char *data_i_1;
 unsigned char *data_i_2;
 unsigned char *data_i_3;
+unsigned char *data_i_4;
+unsigned char *data_i_5;
+unsigned char *data_i_6;
+unsigned char *data_i_7;
 unsigned long int *results_0;
 unsigned long int *results_1;
 unsigned long int *results_2;
 unsigned long int *results_3;
+unsigned long int *results_4;
+unsigned long int *results_5;
+unsigned long int *results_6;
+unsigned long int *results_7;
 unsigned long int *axi_gpio_0;
 unsigned long int *axi_gpio_1;
 unsigned long int *axi_gpio_2;
@@ -77,9 +101,17 @@ unsigned long int *ddr_5;
 unsigned long int *ddr_6;
 unsigned long int *ddr_7;
 unsigned long int *ddr_8;
+unsigned long int *ddr_9;
+unsigned long int *ddr_10;
+unsigned long int *ddr_11;
+unsigned long int *ddr_12;
+unsigned long int *ddr_13;
+unsigned long int *ddr_14;
+unsigned long int *ddr_15;
+unsigned long int *ddr_16;
 unsigned long int idx;
 unsigned long int num_elem;
-unsigned int parallel_units = 4;
+unsigned int parallel_units = 8;
 int a;
 int b;
 int c;
@@ -137,6 +169,14 @@ int open_mem()
     ddr_6 = map_mem(BRAM_BYTES, DDR_6_ADDR);
     ddr_7 = map_mem(BRAM_BYTES, DDR_7_ADDR);
     ddr_8 = map_mem(BRAM_BYTES, DDR_8_ADDR);
+    ddr_9 = map_mem(BRAM_BYTES, DDR_9_ADDR);
+    ddr_10 = map_mem(BRAM_BYTES, DDR_10_ADDR);
+    ddr_11 = map_mem(BRAM_BYTES, DDR_11_ADDR);
+    ddr_12 = map_mem(BRAM_BYTES, DDR_12_ADDR);
+    ddr_13 = map_mem(BRAM_BYTES, DDR_13_ADDR);
+    ddr_14 = map_mem(BRAM_BYTES, DDR_14_ADDR);
+    ddr_15 = map_mem(BRAM_BYTES, DDR_15_ADDR);
+    ddr_16 = map_mem(BRAM_BYTES, DDR_16_ADDR);
 }
 int close_mem()
 {
@@ -195,6 +235,33 @@ void write_i_data()
         !(axi_cdma_3[1] & DONE_CDMA))
     {
     }
+    axi_cdma_0[0] = CLEAR_CDMA;
+    axi_cdma_1[0] = CLEAR_CDMA;
+    axi_cdma_2[0] = CLEAR_CDMA;
+    axi_cdma_3[0] = CLEAR_CDMA;
+    axi_cdma_0[0] = STANDBY_CDMA;
+    axi_cdma_1[0] = STANDBY_CDMA;
+    axi_cdma_2[0] = STANDBY_CDMA;
+    axi_cdma_3[0] = STANDBY_CDMA;
+    axi_cdma_0[6] = DDR_9_ADDR;
+    axi_cdma_0[8] = BRAM_9_ADDR;
+    axi_cdma_1[6] = DDR_11_ADDR;
+    axi_cdma_1[8] = BRAM_11_ADDR;
+    axi_cdma_2[6] = DDR_13_ADDR;
+    axi_cdma_2[8] = BRAM_13_ADDR;
+    axi_cdma_3[6] = DDR_15_ADDR;
+    axi_cdma_3[8] = BRAM_15_ADDR;
+    axi_cdma_0[10] = num_elem;
+    axi_cdma_1[10] = num_elem;
+    axi_cdma_2[10] = num_elem;
+    axi_cdma_3[10] = num_elem;
+    while (
+        !(axi_cdma_0[1] & DONE_CDMA) &&
+        !(axi_cdma_1[1] & DONE_CDMA) &&
+        !(axi_cdma_2[1] & DONE_CDMA) &&
+        !(axi_cdma_3[1] & DONE_CDMA))
+    {
+    }
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     time_write_i += duration.count();
@@ -241,6 +308,33 @@ void read_data()
         !(axi_cdma_3[1] & DONE_CDMA))
     {
     }
+    axi_cdma_0[0] = CLEAR_CDMA;
+    axi_cdma_1[0] = CLEAR_CDMA;
+    axi_cdma_2[0] = CLEAR_CDMA;
+    axi_cdma_3[0] = CLEAR_CDMA;
+    axi_cdma_0[0] = STANDBY_CDMA;
+    axi_cdma_1[0] = STANDBY_CDMA;
+    axi_cdma_2[0] = STANDBY_CDMA;
+    axi_cdma_3[0] = STANDBY_CDMA;
+    axi_cdma_0[6] = BRAM_10_ADDR;
+    axi_cdma_0[8] = DDR_10_ADDR;
+    axi_cdma_1[6] = BRAM_12_ADDR;
+    axi_cdma_1[8] = DDR_12_ADDR;
+    axi_cdma_2[6] = BRAM_14_ADDR;
+    axi_cdma_2[8] = DDR_14_ADDR;
+    axi_cdma_3[6] = BRAM_16_ADDR;
+    axi_cdma_3[8] = DDR_16_ADDR;
+    axi_cdma_0[10] = res_bytes_per_unit;
+    axi_cdma_1[10] = res_bytes_per_unit;
+    axi_cdma_2[10] = res_bytes_per_unit;
+    axi_cdma_3[10] = res_bytes_per_unit;
+    while (
+        !(axi_cdma_0[1] & DONE_CDMA) &&
+        !(axi_cdma_1[1] & DONE_CDMA) &&
+        !(axi_cdma_2[1] & DONE_CDMA) &&
+        !(axi_cdma_3[1] & DONE_CDMA))
+    {
+    }
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     time_read_res += duration.count();
@@ -255,6 +349,10 @@ void print_results()
         res.data[row + pix + 1] = results_1[pix_idx] >> 9;
         res.data[row + pix + 2] = results_2[pix_idx] >> 9;
         res.data[row + pix + 3] = results_3[pix_idx] >> 9;
+        res.data[row + pix + 4] = results_4[pix_idx] >> 9;
+        res.data[row + pix + 5] = results_5[pix_idx] >> 9;
+        res.data[row + pix + 6] = results_6[pix_idx] >> 9;
+        res.data[row + pix + 7] = results_7[pix_idx] >> 9;
         pix_idx++;
     }
     imwrite("/root/hsa-soc-local/img/dices1.jpg", res);
@@ -266,10 +364,18 @@ void set_names()
     data_i_1 = (unsigned char *)ddr_3;
     data_i_2 = (unsigned char *)ddr_5;
     data_i_3 = (unsigned char *)ddr_7;
+    data_i_4 = (unsigned char *)ddr_9;
+    data_i_5 = (unsigned char *)ddr_11;
+    data_i_6 = (unsigned char *)ddr_13;
+    data_i_7 = (unsigned char *)ddr_15;
     results_0 = ddr_2;
     results_1 = ddr_4;
     results_2 = ddr_6;
     results_3 = ddr_8;
+    results_4 = ddr_10;
+    results_5 = ddr_12;
+    results_6 = ddr_14;
+    results_7 = ddr_16;
 }
 int load_image_file()
 {
@@ -324,6 +430,22 @@ void region_of_interest(int x, int y, int unit)
         {
             memcpy(data_i_3, i_img_roi.data, n_times_m);
         }
+        else if (unit == 4)
+        {
+            memcpy(data_i_4, i_img_roi.data, n_times_m);
+        }
+        else if (unit == 5)
+        {
+            memcpy(data_i_5, i_img_roi.data, n_times_m);
+        }
+        else if (unit == 6)
+        {
+            memcpy(data_i_6, i_img_roi.data, n_times_m);
+        }
+        else if (unit == 7)
+        {
+            memcpy(data_i_7, i_img_roi.data, n_times_m);
+        }
     }
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
@@ -356,7 +478,7 @@ int main()
     for (q = 0; q < h_minus_m; q++)
     {
         idx = 0;
-        for (p = 0; p < w_minus_n; p += 4)
+        for (p = 0; p < w_minus_n; p += 8)
         {
             // image parts
             clear_signal();
@@ -364,10 +486,14 @@ int main()
             region_of_interest(p + 1, q, 1);
             region_of_interest(p + 2, q, 2);
             region_of_interest(p + 3, q, 3);
+            region_of_interest(p + 4, q, 4);
+            region_of_interest(p + 5, q, 5);
+            region_of_interest(p + 6, q, 6);
+            region_of_interest(p + 7, q, 7);
             write_i_data();
             work(idx);
             idx += 4;
-            tests += 4;
+            tests += 8;
         }
         // present results
         read_data();
