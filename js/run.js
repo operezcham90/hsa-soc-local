5f9e269f9ef9c988a84377440fcd5aac6a9fa69f
operezcham <operezcham@gmail.com>
2021-07-11 13:00:58 -0500

    json

--- js/run.js
+++ js/run.js
@@ -1,4 +1,5 @@
 const { exec } = require('child_process');
+var categories = [];
 function mount_drive() {
     exec('mount /dev/sda1 /mnt', (error, stdout, stderr) => {
         check_drive();
@@ -6,7 +7,8 @@ function mount_drive() {
 }
 function check_drive() {
     exec('ls /mnt/alov/ann', (error, stdout, stderr) => {
-        console.log('folders:\n' + stdout);
+        categories = stdout.split('\n');
+        console.log(JSON.stringify(categories));
     });
 }
 mount_drive();
