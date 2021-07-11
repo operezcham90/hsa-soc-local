37b98a5b38bce9831e5b7264e3a89508a24a883d
operezcham <operezcham@gmail.com>
2021-07-11 13:23:02 -0500

    video

--- js/run.js
+++ js/run.js
@@ -1,5 +1,7 @@
 const { exec } = require('child_process');
 var categories = [];
+var videos = [];
+var video_count = 0;
 function mount_drive() {
     exec('mount /dev/sda1 /mnt', (error, stdout, stderr) => {
         check_drive();
@@ -8,7 +10,21 @@ function mount_drive() {
 function check_drive() {
     exec('ls /mnt/alov/ann', (error, stdout, stderr) => {
         categories = stdout.split('\n');
-        console.log(JSON.stringify(categories));
+        categories.pop();
+        check_folder(0);
+    });
+}
+function check_folder(category) {
+    var folder = '/mnt/alov/ann/' + categories[category];
+    exec('ls ' + folder, (error, stdout, stderr) => {
+        var folder_videos = stdout.split('\n');
+        folder_videos.pop();
+        videos.push(folder_videos);
+        video_count += folder_videos.length;
+        console.log(video_count);
+        if (category < categories.length) {
+            check_folder(category++);
+        }
     });
 }
 mount_drive();
