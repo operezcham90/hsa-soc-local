7d7855a318f5ae6c49fd97bba94cd8e3e24f063e
operezcham <operezcham@gmail.com>
2021-07-11 12:49:52 -0500

    no message

--- js/run.js
+++ js/run.js
@@ -1,9 +1,7 @@
 const { spawn } = require("child_process");
 
 const mount = spawn("mount", ["/dev/sda1", "/mnt"]);
-mount.stdout.on("data", data => {
-    const ls = spawn("ls", ["/mnt/alov"]);
-    ls.stdout.on("data", data => {
-        console.log(data);
-    });
+const ls = spawn("ls", ["/mnt/alov"]);
+ls.stdout.on("data", data => {
+    console.log(data);
 });
