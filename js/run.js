5beeb063849144187401eb820ab917f43609947e
operezcham <operezcham@gmail.com>
2021-07-11 01:17:25 -0500

    order

--- js/run.js
+++ js/run.js
@@ -1,8 +1,8 @@
 const { spawn } = require("child_process");
 
-const umount = await spawn("umount /dev/sda1");
+const umount = spawn("umount /dev/sda1");
 umount.stdout.on("data", data => {
-    const mount = await spawn("mount /dev/sda1 /mnt");
+    const mount = spawn("mount /dev/sda1 /mnt");
     mount.stdout.on("data", data => {
         console.log(data);
     });
