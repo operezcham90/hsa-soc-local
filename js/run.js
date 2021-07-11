c17253c5afccffe3fcaccc164bdd1ad5bec5b50b
operezcham <operezcham@gmail.com>
2021-07-11 12:57:06 -0500

    folders

--- js/run.js
+++ js/run.js
@@ -1,9 +1,12 @@
 const { exec } = require('child_process');
-exec('mount /dev/sda1 /mnt', (error, stdout, stderr) => {
-    if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-});
+function mount_drive() {
+    exec('mount /dev/sda1 /mnt', (error, stdout, stderr) => {
+        check_drive();
+    });
+}
+function check_drive() {
+    exec('ls /mnt/alov/ann', (error, stdout, stderr) => {
+        console.log('folders:\n' + stdout);
+    });
+}
+mount_drive();
