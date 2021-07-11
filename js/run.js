5f7a8d4885aa368cf4b16789b4b23b0289f2ef44
operezcham <operezcham@gmail.com>
2021-07-11 12:53:01 -0500

    exec

--- js/run.js
+++ js/run.js
@@ -1,7 +1,9 @@
-const { spawn } = require("child_process");
-
-const mount = spawn("mount", ["/dev/sda1", "/mnt"]);
-const ls = spawn("ls", ["/mnt/alov"]);
-ls.stdout.on("data", data => {
-    console.log(data);
+const { exec } = require('child_process');
+exec('mount /dev/sda1 /mnt', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
 });
