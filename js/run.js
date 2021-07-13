e6b0182840ce373061e4da7812d525708b25c5f6
operezcham <operezcham@gmail.com>
2021-07-12 22:06:45 -0500

    score

--- js/run.js
+++ js/run.js
@@ -10,6 +10,11 @@ var top_l_x = 0;
 var top_l_y = 0;
 var bottom_r_x = 0;
 var bottom_r_y = 0;
+var ann = [];
+
+var ntp;
+var nfp;
+var nfn;
 
 function mount_drive() {
     exec('mount /dev/sda1 /mnt', (error, stdout, stderr) => {
@@ -51,54 +56,39 @@ function read_ann(category, video) {
         last_frame_index = +last_frame_data[0];
         console.log(categories[category] + ' ' + video + ' ' + first_frame_index + '-' + last_frame_index);
 
-        top_l_x = +first_frame_data[1];
-        if (+first_frame_data[3] < top_l_x) {
-            top_l_x = +first_frame_data[3];
-        }
-        if (+first_frame_data[5] < top_l_x) {
-            top_l_x = +first_frame_data[5];
-        }
-        if (+first_frame_data[7] < top_l_x) {
-            top_l_x = +first_frame_data[7];
-        }
-
-        top_l_y = +first_frame_data[2];
-        if (+first_frame_data[4] < top_l_y) {
-            top_l_y = +first_frame_data[4];
-        }
-        if (+first_frame_data[6] < top_l_y) {
-            top_l_y = +first_frame_data[6];
-        }
-        if (+first_frame_data[8] < top_l_y) {
-            top_l_y = +first_frame_data[8];
-        }
-
-        bottom_r_x = +first_frame_data[1];
-        if (+first_frame_data[3] > bottom_r_x) {
-            bottom_r_x = +first_frame_data[3];
-        }
-        if (+first_frame_data[5] > bottom_r_x) {
-            bottom_r_x = +first_frame_data[5];
-        }
-        if (+first_frame_data[7] > bottom_r_x) {
-            bottom_r_x = +first_frame_data[7];
-        }
-
-        bottom_r_y = +first_frame_data[2];
-        if (+first_frame_data[4] > bottom_r_y) {
-            bottom_r_y = +first_frame_data[4];
-        }
-        if (+first_frame_data[6] > bottom_r_y) {
-            bottom_r_y = +first_frame_data[6];
-        }
-        if (+first_frame_data[8] > bottom_r_y) {
-            bottom_r_y = +first_frame_data[8];
-        }
+        top_l_x = Math.min(+first_frame_data[1], +first_frame_data[3], +first_frame_data[5], +first_frame_data[7]);
+        top_l_y = Math.min(+first_frame_data[2], +first_frame_data[4], +first_frame_data[6], +first_frame_data[8]);
+        bottom_r_x = Math.max(+first_frame_data[1], +first_frame_data[3], +first_frame_data[5], +first_frame_data[7]);
+        bottom_r_y = Math.max(+first_frame_data[2], +first_frame_data[4], +first_frame_data[6], +first_frame_data[8]);
 
         top_l_x = Math.round(top_l_x);
         top_l_y = Math.round(top_l_y);
         bottom_r_x = Math.round(bottom_r_x);
         bottom_r_y = Math.round(bottom_r_y);
+
+        for (var i = 0; i <= frames.length - 2; i++) {
+            var line = frames[0].split(/\s/g);
+            var gt = {
+                frame: +line[0],
+                ax: +line[1],
+                ay: +line[2],
+                bx: +line[3],
+                by: +line[4],
+                cx: +line[5],
+                cy: +line[6],
+                dx: +line[7],
+                dy: +line[8]
+            };
+            gt.u = Math.min(gt.ax, gt.bx, gt.cx, gt.dx);
+            gt.v = Math.min(gt.ay, gt.by, gt.cy, gt.dy);
+            gt.w = Math.max(gt.ax, gt.bx, gt.cx, gt.dx) - gt.u;
+            gt.h = Math.max(gt.ay, gt.by, gt.cy, gt.dy) - gt.v;
+            ann.push(gt);
+        }
+
+        ntp = 1;
+        nfp = 0;
+        nfn = 0;
 
         set_frame_run(category, video, first_frame_index + 1);
     });
@@ -131,6 +121,35 @@ function do_frame_run(category, video, current_frame) {
                         bottom_r_x = top_l_x + n;
                         bottom_r_y = top_l_y + m;
                         console.log('u,v: ' + top_l_x + ',' + top_l_y);
+
+                        // f-score
+                        for (var i = 0; i < ann.length; i++) {
+                            if (ann[i].frame == current_frame) {
+                                var gt = ann[i];
+                                var t = {
+                                    u: top_l_x,
+                                    v: top_l_y,
+                                    w: n,
+                                    h: m
+                                };
+                                var x_overlap = Math.max(0, Math.min(t.u + t.w, gt.u + gt.w) - Math.max(t.u, gt.u));
+                                var y_overlap = Math.max(0, Math.min(t.v + t.h, gt.v + gt.h) - Math.max(t.v, gt.v));
+                                var intersection = x_overlap * y_overlap;
+                                var union = (t.w * t.h) + (gt.w * gt.h) - intersection;
+                                var iou = intersection / union;
+                                var pascal = iou >= 0.5;
+                                if (pascal) {
+                                    ntp++;
+                                } else {
+                                    nfp++;
+                                }
+                                var precision = ntp / (ntp + nfp);
+                                var recall = ntp / (ntp + nfn);
+                                var f = (2 * precision * recall) / (precision + recall);
+                                console.log('F: ' + f);
+                            }
+                        }
+
                         var res = '/mnt/alov/res' + categories[category] + '_' + ('00000' + (video + 1)).slice(-5) + '_' + ('00000000' + (current_frame)).slice(-8);
                         fs.writeFile(res, stdout, (err) => {
                             if (current_frame + 1 <= last_frame_index) {
