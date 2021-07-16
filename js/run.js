fb4a6cf04b6c982b65b05d907f8ffdaf15e903c6
operezcham <operezcham@gmail.com>
2021-07-16 17:02:21 -0500

    continue

--- js/run.js
+++ js/run.js
@@ -1,9 +1,9 @@
 const exec = require('child_process').exec;
 const fs = require('fs');
 
-const result_file = '/root/hsa-soc-local/cpp/result2.csv';
+const result_file = '/root/hsa-soc-local/cpp/result3.csv';
 const start_cat = 1;
-const start_vid = 1;
+const start_vid = 11;
 
 var categories = [];
 var videos = [];
