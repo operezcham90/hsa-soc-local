8a80ce544264352441a9a761770b246fe8cd9f7d
operezcham <operezcham@gmail.com>
2021-07-17 02:54:35 -0500

    path

--- js/boss.js
+++ js/boss.js
@@ -1,6 +1,10 @@
+//pm2 start hsa-soc-local/js/boss.js
+//pm2 save
+//pm2 startup
+
 const exec = require('child_process').exec;
 
-const command_work = 'node work.js';
+const command_work = 'node /root/hsa-soc-local/js/work.js';
 
 function work() {
     exec(command_work, (error, stdout, stderr) => {
